1876c465c80863a63f41ae3941d4903c2e5fe144
John R. Starr <johnstarr1997@gmail.com>
2022-12-10 13:18:27 -0500

    add: hw5 files

---
+++ miniconsbatched.py
@@ -0,0 +1,46 @@
+import numpy as np
+from minicons import cwe
+import torch
+
+def generate_representations(words: list, contexts: list, 
+                            model: str = 'bert-base-cased', 
+                            batch_size: int = 8,
+                            device: str = 'cpu',
+                            layer=None) -> (np.ndarray, np.ndarray):
+    """Returns representations and word-context pairs as a tuple of ndarrays.
+       words: list-like of words.
+       contexts: dict of {word: context} pairs.
+       model: valid huggingface model.
+       batch_size: desired size of batch (default is 8)
+       device: device to run inference (default is cuda)
+       
+       returns: word representation vectors of shape (|words|, |vector|), 
+                word-context pairs."""
+
+    print('########################################')
+    print('Running', model, 'for layer', str(layer), '!')
+    model = cwe.CWE(model, device=device)
+    batch_ranges = list(range(0, len(words), batch_size))
+    batch_ranges.append(len(words))
+    pairs = np.array(list(zip(contexts, words)))
+    vectors = []
+
+    for i in range(1, len(batch_ranges)):
+        ix_range = (batch_ranges[i-1], batch_ranges[i])
+        # vectors.extend(model.extract_representation(pairs[ix_range[0]: ix_range[1]], layer=layer).numpy())
+    
+        # Multiple layers
+        if type(layer)!=int:
+            reps = model.extract_representation(pairs[ix_range[0]: ix_range[1]], layer=layer)
+            reps = np.array([layer_reps.numpy() for layer_reps in reps])
+
+        # Single layer
+        else:
+            reps = model.extract_representation(pairs[ix_range[0]: ix_range[1]], layer=layer).numpy()
+        
+        vectors.extend(reps)
+    print('Run complete.')
+    print()
+    return np.array(vectors), np.array(pairs)
+
+
